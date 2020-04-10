f925424f948dddfb983b84e8526d00518c138c70
incremental capital <incrementalcapital@gmail.com>
2020-04-10 16:44:40 -0500

    Get trade information.

---
+++ weth-dai-trading/get-my-trades.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import json
+
+import dydx.constants as consts
+import dydx.util as utils
+
+from credentials import client
+
+
+# Get trades created by my account for both sides of the orderbook
+my_trades = client.get_my_trades(
+    market=['WETH-DAI'],
+    limit=None,  # optional
+    startingBefore=None  # optional
+)
+
+jsondata = json.dumps( my_trades, sort_keys=True, indent=4, separators=(',', ': ') )
+
+print ( jsondata )
