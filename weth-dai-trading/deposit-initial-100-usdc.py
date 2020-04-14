611588ddb82a6abcb38df860fbf33cd3bc60da3d
incremental capital <incrementalcapital@gmail.com>
2020-04-14 15:20:16 -0500

    Use toJSON method from Web3 library.

--- weth-dai-trading/deposit-initial-100-usdc.py
+++ weth-dai-trading/deposit-initial-100-usdc.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+
+import json
+from web3 import Web3
 from decimal import Decimal
 
 from dydx.client import Client
@@ -10,17 +13,17 @@ from credentials import client
 
 
 # Set Allowance [must only be called once EVER (aka the initial deposit)]
-tx_hash = client.eth.set_allowance(market=consts.MARKET_USDC)
-receipt = client.eth.get_receipt(tx_hash)
+tx_hash = client.eth.set_allowance( market=consts.MARKET_USDC )
+receipt = client.eth.get_receipt( tx_hash )
 
 depositamount = Decimal("100")
 
 # Deposit USDC
 tx_hash = client.eth.deposit(
   market=consts.MARKET_USDC,
-  wei=utils.token_to_wei(depositamount, consts.MARKET_USDC)
+  wei=utils.token_to_wei( depositamount, consts.MARKET_USDC )
 )
-receipt = client.eth.get_receipt(tx_hash)
-
-# Display deposit confirmation
-print ( receipt )
+# Display deposit information
+receipt = client.eth.get_receipt( tx_hash )
+web3out = Web3.toJSON( receipt )
+print( web3out )
