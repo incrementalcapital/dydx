a7d2dab8b08106632d9d7f688562beba11cceccf
incremental capital <incrementalcapital@gmail.com>
2020-04-14 15:11:15 -0500

    Use Web3.toJSON to handle the output.

--- weth-dai-trading/withdraw-100-usdc.py
+++ weth-dai-trading/withdraw-100-usdc.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+
+import json
+from web3 import Web3
 from decimal import Decimal
 
 from dydx.client import Client
@@ -10,13 +13,14 @@ from credentials import client
 
 
 # Specify withdraw amount
-withdrawalamount = Decimal("100")
+withdrawalamount = Decimal( "100" )
 
 # Withdraw USDC
 tx_hash = client.eth.withdraw(
   market=consts.MARKET_USDC,
-  wei=utils.token_to_wei(withdrawalamount, consts.MARKET_USDC)
+  wei=utils.token_to_wei( withdrawalamount, consts.MARKET_USDC )
 )
-receipt = client.eth.get_receipt(tx_hash)
-
-print ( receipt )
+# Display withdrawal information
+receipt = client.eth.get_receipt( tx_hash )
+web3out = Web3.toJSON( receipt )
+print( web3out )
