90e6f8477bfe75494c95bedd4cde130a84d4ac13
incremental capital <incrementalcapital@gmail.com>
2020-04-16 13:41:09 -0500

    Create a logger module, a test for it, and improve the formatting of the messages output.

---
+++ weth-dai-trading/testlogger.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+from logger import logger
+
+
+# Generate log messages
+logger.debug('This is a debug message.')
+logger.info('This is an info message.')
+logger.warning('This is a warning message.')
+logger.error('This is an error message.')
+logger.critical('This is a critical message.')
