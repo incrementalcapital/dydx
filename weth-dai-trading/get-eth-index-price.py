57b591dbf2d8a6627c4073055e3b956a9ad8f124
incremental capital <incrementalcapital@gmail.com>
2020-04-19 08:54:16 -0500

    Use dYdX provided constants. Update the script to use solo inkeeping with the latest version of the dYdX Python library.

--- weth-dai-trading/get-eth-index-price.py
+++ weth-dai-trading/get-eth-index-price.py
@@ -9,15 +9,9 @@ import dydx.util as utils
 from credentials import client
 
 
-# Get WETH-DAI market information
-markets = client.get_markets()
-asset = markets["markets"]["WETH-DAI"]["baseCurrency"]["currency"]
-decimals = markets["markets"]["WETH-DAI"]["baseCurrency"]["decimals"]
-assetid = markets["markets"]["WETH-DAI"]["baseCurrency"]["soloMarketId"]
-
 # Get dYdX index price for ETH
-rawdata = client.eth.get_oracle_price( assetid )
-pricedata = Decimal(rawdata) * Decimal( 10**(decimals) )
+rawdata = client.eth.solo.get_oracle_price( consts.MARKET_WETH )
+pricedata = Decimal(rawdata) * Decimal( 10**(consts.DECIMALS_WETH) )
 indexprice = Decimal(pricedata)
 
 # Format balance using DECIMAL information for the asset
