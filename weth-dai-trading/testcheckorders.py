5831a880941087fa82cce66343c963376a38e59d
incremental capital <incrementalcapital@gmail.com>
2020-04-30 13:19:11 -0500

    Rename test.

---
+++ weth-dai-trading/testcheckorders.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+import asyncio
+
+from websocketconnector import checkorders
+
+asyncio.run( checkorders( "0xba91f6624a9baba2f74e99e248883a5ab7911b6ab1be534ad25406ff4fb28106" ) )
--- weth-dai-trading/testordersfulfilledstream.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-import asyncio
-
-from websocketconnector import ordersfulfilledstream
-
-asyncio.run( ordersfulfilledstream( "0xba91f6624a9baba2f74e99e248883a5ab7911b6ab1be534ad25406ff4fb28106" ) )
