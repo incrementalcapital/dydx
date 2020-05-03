02b384121c9828b56d8e01106a371f6cdd83cd88
incremental capital <incrementalcapital@gmail.com>
2020-05-03 16:12:12 -0500

    Update orderer library to use logging and execption handling. Improved reporting.

--- weth-dai-trading/orderer.py
+++ weth-dai-trading/orderer.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 
 
+import json
+import logging
 from decimal import Decimal
 
 from dydx.client import Client
 import dydx.constants as consts
 import dydx.util as utils
 
+from logger import logger
 from credentials import client
 
 # Define best price function
@@ -36,28 +39,62 @@ def bestorders( tradingpair, quotetick ):
 # Define post-only ask
 # In other words, limit ask
 def postask( price, quantity ):
-        # Create order to SELL ETH
-        placed_ask = client.place_order(
+
+    logger.debug ( f'Asking {price} DAI/ETH for {quantity} ETH.' )
+    ask = Decimal(price)
+    amount = Decimal(quantity)
+
+    # Ask.
+    try:
+        # Submit order to dYdX.
+        submission = client.place_order(
             market=consts.PAIR_WETH_DAI,
             side=consts.SIDE_SELL,
-            amount=utils.token_to_wei(quantity, consts.MARKET_WETH),
-            price=price,
+            amount=utils.token_to_wei(amount, consts.MARKET_WETH),
+            price=ask,
             fillOrKill=False,
             postOnly=True
         )
-        return placed_ask
+    except Exception as e:
+        # Throw a critical error notice if anything funky occurs.
+        logger.critical(f'{e}', exc_info=True)
+        return 'ERROR'
+
+    # Write the dYdX response to the submission to the logs.
+    jsondata = json.dumps( submission, sort_keys=True, indent=4, separators=(',', ': ') )
+    logger.debug ( f'Order submission:\n{jsondata}' )
+
+    # Return results of the submission to BUY ETH
+    return submission
 
 
 # Define post-only bid
 # In other words, limit bid
 def postbid( price, quantity ):
-        # Submit the order to BUY ETH
-        placed_bid = client.place_order(
+
+    logger.debug ( f'Bidding at {price} DAI/ETH for {quantity} ETH.' )
+    bid = Decimal(price)
+    amount = Decimal(quantity)
+
+    # Bid.
+    try:
+        # Submit order to dYdX.
+        submission = client.place_order(
             market=consts.PAIR_WETH_DAI,
             side=consts.SIDE_BUY,
-            amount=utils.token_to_wei(quantity, consts.MARKET_WETH),
-            price=price,
+            amount=utils.token_to_wei(amount, consts.MARKET_WETH),
+            price=bid,
             fillOrKill=False,
             postOnly=True
         )
-        return placed_bid
+    except Exception as e:
+        # Throw a critical error notice if anything funky occurs.
+        logger.critical(f'{e}', exc_info=True)
+        return 'ERROR'
+
+    # Write the dYdX response to the submission to the logs.
+    jsondata = json.dumps( submission, sort_keys=True, indent=4, separators=(',', ': ') )
+    logger.debug ( f'Order submission:\n{jsondata}' )
+
+    # Return results of the submission to BUY ETH
+    return submission
