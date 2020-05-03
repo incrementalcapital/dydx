32316aeeecd03835360688db10b43d3e4743eb8c
incremental capital <incrementalcapital@gmail.com>
2020-05-03 16:10:15 -0500

    Add withdrawal scripts to wrappers. All seem to working. However, more testing needed.

---
+++ weth-dai-trading/wrappers/withdraw-100-dai.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import sys
+import json
+
+from web3 import Web3
+
+from dydx.client import Client
+import dydx.constants as consts
+import dydx.util as utils
+
+from credentials import client
+
+# Specify withdraw amount
+withdrawalamount = 100
+
+try:
+    # Withdraw all DAI
+    withdrawalhash = client.eth.solo.withdraw(
+        market=consts.MARKET_DAI,
+        wei=utils.token_to_wei( withdrawalamount, consts.MARKET_DAI )
+    )
+
+except Exception as e:
+    # Throw a critical error notice if anything funky occurs
+    print ( f'Exception occurred:\n\n{e}\n' )
+    sys.exit(0)
+
+# Display withdrawal information
+receipt = client.eth.get_receipt( withdrawalhash )
+web3out = Web3.toJSON( receipt )
+strings = str( web3out )
+dataout = json.loads( strings )
+jsonout = json.dumps( dataout, sort_keys=True, indent=4, separators=(',', ': ') )
+print (jsonout)
---
+++ weth-dai-trading/wrappers/withdraw-all-weth.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import sys
+import json
+
+from web3 import Web3
+
+from dydx.client import Client
+import dydx.constants as consts
+import dydx.util as utils
+
+from credentials import client
+
+
+try:
+    # Withdraw all DAI
+    withdrawalhash = client.eth.solo.withdraw_to_zero( market=consts.MARKET_WETH )
+
+except Exception as e:
+    # Throw a critical error notice if anything funky occurs
+    print ( f'Exception occurred:\n\n{e}\n' )
+    sys.exit(0)
+
+# Display withdrawal information
+receipt = client.eth.get_receipt( withdrawalhash )
+web3out = Web3.toJSON( receipt )
+strings = str( web3out )
+dataout = json.loads( strings )
+jsonout = json.dumps( dataout, sort_keys=True, indent=4, separators=(',', ': ') )
+print (jsonout)
