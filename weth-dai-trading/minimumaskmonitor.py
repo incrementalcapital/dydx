3dee6b6c64706bcd56049f6aaae41b8310008637
incremental capital <incrementalcapital@gmail.com>
2020-05-13 16:00:24 -0500

    Improve the broken logic of the minimumaskhandler.

--- weth-dai-trading/minimumaskmonitor.py
+++ weth-dai-trading/minimumaskmonitor.py
@@ -78,40 +78,41 @@ async def minimumaskmessagehandler(
                     # Display price updates.
                     logger.debug( f'updated information received... [lower price bound / upper price bound : {lowerlimit:.2f}/{upperlimit:.2f} DAI/ETH] the lowest ask in the orderbook is: {Decimal(minimumask):.2f} DAI/ETH [Message ID: {dictionary["message_id"]}].' )
 
-                    # Depreciation Logic
-                    #
-                    # [This logic handles INCREASES in the minimum ask price as long as it does not fall below the depreciation trigger.]
-                    #
                     # Use the following logic if there is an initial minimum price.
-                    # Set the lower exit price to the depreciation trigger price.
-                    # This is calculated once. It is determined in the first update message.
-                    # The minimum ask must rise above this initial minimum price.
-                    # Exit if the minimum ask drops below the upwardly sliding lower limit.
                     if Decimal(initialminimumprice):
-                        if lowerlimit == float("-inf"): lowerlimit = Decimal(minimumask) * ( 1 - Decimal(percentdepreciation) )
-                        if Decimal(minimumask) * ( 1 - Decimal(percentdepreciation) ) > Decimal(initialminimumprice):
-                            if Decimal(minimumask) * ( 1 - Decimal(percentdepreciation) ) < lowerlimit:
+
+                        # Define lagging ask price.
+                        laggingask = Decimal(minimumask) * ( 1 - Decimal(percentdepreciation) )
+
+                        # Execute the following when the lagging ask exceeds the initial minimum ask specified.
+                        if laggingask > Decimal(initialminimumprice):
+
+                            # Increase the lower exit price if the lagging ask continues to increase.
+                            # Otherwise, close the connection if the minimum ask drops below the upwardly sliding lower limit.
+                            if laggingask < lowerlimit:
                                 logger.debug( f'The lowest ask [{Decimal(minimumask):.2f} DAI/ETH] in the orderbook just dropped below a {Decimal(percentdepreciation)*100:.2f}% margin over the lower price bound [{lowerlimit:.2f} DAI/ETH].' )
                                 killsocket = True
                             else:
-                                lowerlimit = Decimal(minimumask) * ( 1 - Decimal(percentdepreciation) )
-                    # Appreciation Logic
-                    #
-                    # [This logic handles DECREASES in the minimum ask price as long as it does not rise below the appreciation trigger.]
-                    #
+                                # Set the lower exit price to the depreciation trigger price.
+                                lowerlimit = laggingask
+
                     # Use the following logic if there is an initial maximum price.
-                    # Set the upper exit price to the appreciation trigger price.
-                    # This is calculated once. It is determined in the first update message.
-                    # The minimum ask must fall below above this initial maximum price.
-                    # Exit if the minimum ask rises above the downwardly sliding upper limit.
                     elif Decimal(initialmaximumprice):
-                        if upperlimit == float("inf"): upperlimit = Decimal(minimumask) * ( 1 + Decimal(percentappreciation) )
-                        if Decimal(minimumask) * ( 1 + Decimal(percentappreciation) ) < Decimal(initialmaximumprice):
-                            if Decimal(minimumask) * ( 1 + Decimal(percentappreciation) ) > upperlimit:
+
+                        # Define leading ask price.
+                        leadingask = Decimal(minimumask) * ( 1 + Decimal(percentappreciation) )
+
+                        # Execute the following when the leading ask drops below the initial minimum ask specified.
+                        if leadingask < Decimal(initialmaximumprice):
+
+                            # Decrease the upper exit price if the lagging ask continues to decrease.
+                            # Otherwise, close the connection if the minimum ask exceeds the downwardly sliding upper limit.
+                            if leadingask > upperlimit:
                                 logger.debug( f'The lowest ask [{Decimal(minimumask):.2f} DAI/ETH] in the orderbook just exceeded a {percentappreciation*100:.2f}% margin below the upper price bound [{upperlimit:.2f} DAI/ETH].' )
                                 killsocket = True
                             else:
-                                upperlimit = Decimal(minimumask) * ( 1 + Decimal(percentappreciation) )
+                                upperlimit = leadingask
+
                     else:
                         # If the desired percentage depreciation is set to zero (FALSE), let the lower exit price remain at negative infinity.
                         # Otherwise (percentdepreciation is TRUE), use any increase in minimum ask to determine a new lower price exit.
